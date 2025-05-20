fdd7bc47f2f2008adeda4b0854ff2a7feec3cbf9
Fatemeh A-Mohammadi <129193825+FatemehMohammadi18@users.noreply.github.com>
2025-05-20 14:07:51 +0330

    create-and-route-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth";
+import PanelAdmin from "./pages/panelAdmin";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/panelAdmin" element={<PanelAdmin />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
